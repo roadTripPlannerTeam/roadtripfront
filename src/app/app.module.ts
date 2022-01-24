638810514516587eb929f4f91ab30b1a715e5512
Brice Dobrowolski <brice.dobrowolski@yahoo.fr>
2022-01-24 23:00:43 +0100

    signup css done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { FormSignupComponent } from './form-signup/form-signup.component';
 import { ConfirmButtonComponent } from './confirm-button/confirm-button.component';
 import { NotYetSignupComponent } from './not-yet-signup/not-yet-signup.component';
+import { HeaderAuthComponent } from './header-auth/header-auth.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormSignupComponent,
     ConfirmButtonComponent,
-    NotYetSignupComponent
+    NotYetSignupComponent,
+    HeaderAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
