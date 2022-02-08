398602f41714dbbcea72265fad2b8eb40c7f47ba
vnstsn <va.tsang@yahoo.fr>
2022-02-08 09:30:37 +0100

    init manage trip pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ManageTripComponent } from './manage-page/page/manage-trip/manage-trip.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "managetrip", component: ManageTripComponent},
+  //{path: "todolist", component: },
+  //{path: "budget", component:},
+  //{path: "conversion", component:},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ConversionComponent } from './manage-page/conversion/conversion.component';
+
+import {TabViewModule} from 'primeng/tabview';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+
+
+import { TabviewManageComponent } from './manage-page/tabview-manage/tabview-manage.component';
+import { TodolistComponent } from './manage-page/todolist/todolist.component';
+import { BudgetComponent } from './manage-page/budget/budget.component';
+import { ViewBudgetComponent } from './manage-page/view-budget/view-budget.component';
+import { ManageTripComponent } from './manage-page/page/manage-trip/manage-trip.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TodolistFormComponent } from './manage-page/todolist-form/todolist-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderInformationComponent } from './header-information/header-information.component';
+import { AffichageInformationComponent } from './affichage-information/affichage-information.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversionComponent,
+    TabviewManageComponent,
+    TodolistComponent,
+    BudgetComponent,
+    ViewBudgetComponent,
+    ManageTripComponent,
+    TodolistFormComponent,
+    HeaderInformationComponent,
+    AffichageInformationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTabsModule,
+    InputTextareaModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
